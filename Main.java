6360af6b67af07fc0d176e0d81bd4d30ece85119
putriayusushmita <putriayusushmita6@gmail.com>
2021-10-21 22:08:07 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,49 @@
+class Main {
+    public static void main(String[] args) {
+      Heroes hero = new Heroes();
+  
+      hero.setProfile("Putri Ayu Sushmita", 20, 155);
+      hero.profile();
+      
+  
+      // jalankan aktivitas disini...
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+      hero.jalan();
+  
+      hero.lari();
+      hero.lari();
+      hero.lari();
+      hero.lari();
+
+      hero.makan();
+      hero.makan();
+      hero.makan();
+      hero.makan();
+
+      hero.minum();
+      hero.minum();
+      hero.minum();
+      hero.minum();
+
+      hero.lompat();
+      hero.lompat();
+      hero.lompat();
+
+      hero.duduk();
+      hero.duduk();
+      
+
+
+
+
+
+
+      
+      // tampilkan status terkini...
+      hero.profile();
+      
+    }
+  }
